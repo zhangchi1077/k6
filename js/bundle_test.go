bcf425090f5c81f1779980172cab20ce5c94a3ef
Mihail Stoykov <M.Stoikov@gmail.com>
2019-03-21 17:47:19 +0200

    Run with windosified paths as well

--- js/bundle_test.go
+++ js/bundle_test.go
@@ -487,17 +487,16 @@ func TestOpen(t *testing.T) {
 			fs = afero.NewReadOnlyFs(fs)
 			for _, tCase := range testCases {
 				tCase := tCase
-				var openPath = tCase.openPath
-				// if fullpath prepend prefix
-				if openPath[0] == '/' || openPath[0] == '\\' {
-					openPath = filepath.Join(prefix, openPath)
-				}
-				if runtime.GOOS == "windows" {
-					openPath = strings.Replace(openPath, `\`, `\\`, -1)
-				}
-				spew.Dump(openPath)
 
-				t.Run(tCase.name, func(t *testing.T) {
+				var testFunc = func(t *testing.T) {
+					var openPath = tCase.openPath
+					// if fullpath prepend prefix
+					if openPath[0] == '/' || openPath[0] == '\\' {
+						openPath = filepath.Join(prefix, openPath)
+					}
+					if runtime.GOOS == "windows" {
+						openPath = strings.Replace(openPath, `\`, `\\`, -1)
+					}
 					src := &lib.SourceData{
 						Filename: filepath.Join(prefix, "/path/to/script.js"),
 						Data: []byte(`
@@ -533,7 +532,15 @@ func TestOpen(t *testing.T) {
 							assert.Equal(t, "hi", v.Export())
 						})
 					}
-				})
+				}
+
+				t.Run(tCase.name, testFunc)
+				if runtime.GOOS == "windows" {
+					// windowsify the testcase
+					tCase.openPath = strings.Replace(tCase.openPath, `/`, `\`, -1)
+					tCase.pwd = strings.Replace(tCase.pwd, `/`, `\`, -1)
+					t.Run(tCase.name+" with windows slash", testFunc)
+				}
 			}
 		})
 	}
